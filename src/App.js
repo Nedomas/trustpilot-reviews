70b1696ff8c57e719cc05d2fab5cb2e4a9547c6d
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-03-03 13:26:55 +0200

    add badReview state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, { Component } from 'react';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+import React, { Component } from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      badReview: false
+    };
+  }
+  handleBadReview() {
+    this.setState({
+      badReview: true
+    });
+  }
   render() {
+    const { badReview } = this.state;
+
     return (
       <div>
         <header>
           <h1>TRUSTUM.io</h1>
         </header>
-        <MessengerCustomerChat
-          pageId='369078686897983'
-          appId='369233150149061'
-        />
+        <button onClick={() => this.handleBadReview()}>This is shit</button>
+        {badReview && (
+          <MessengerCustomerChat
+            pageId="369078686897983"
+            appId="369233150149061"
+          />
+        )}
       </div>
     );
   }
