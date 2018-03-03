1e090943bf8bd8485552552c1c870236fa935bf3
Domas <domas.bitvinskas@me.com>
2018-03-03 13:36:56 +0200

    Use components dir

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+import React, { Component } from "react";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      badReview: false
+    };
+  }
+  handleBadReview() {
+    this.setState({
+      badReview: true
+    });
+  }
   render() {
+    const { badReview } = this.state;
+
     return (
-      <div style={styles.container}>
-        <h1>TRUSTUM.io</h1>
-        <MessengerCustomerChat
-          pageId='369078686897983'
-          appId='369233150149061'
-        />
+      <div>
+        <header>
+          <h1>TRUSTUM.io</h1>
+        </header>
+        <button onClick={() => this.handleBadReview()}>This is shit</button>
+        {badReview && (
+          <MessengerCustomerChat
+            pageId="369078686897983"
+            appId="369233150149061"
+          />
+        )}
       </div>
     );
   }
 }
-
-const styles = {
-  container: {
-    paddingTop: '100px',
-    textAlign: 'center',
-  },
-};
 
 export default App;
