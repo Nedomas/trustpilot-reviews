fd9fc5155ef963f4146259a1ebe01c1533beffd2
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-03-04 00:03:33 +0200

    add info block to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -29,11 +29,11 @@ class Header extends Component {
         <div className="header--content">
           <div className="header--info">
             <h2 className="header--title">
-              Have you ever dealt with bad news?
+              We automate and simplify dealing with negative reviews.
             </h2>
             <p className="header--text">
-              What if we told you that those days are over and we're ready to
-              help you out with negativity?
+              Our chatbot finds, analyzes and automatically responds to negative
+              reviews for you, real time.
             </p>
             <button className="button">
               <span>LEARN MORE</span>
@@ -50,7 +50,46 @@ class Header extends Component {
 
 class Info extends Component {
   render() {
-    return <div className="info" />;
+    return (
+      <div className="info">
+        <h2>We help you find out where the problem is and appease clients.</h2>
+        <p>
+          Did you know that one negative review can cost you up to thirty
+          customers? We help you prevent negative reviews as soon as they happen
+          by finding all the reviews for you, quickly identifying the cause of
+          the review, and messaging the user to collect more relevant details.
+        </p>
+        <div className="info--cards">
+          <div className="info--card">
+            <img src="/icon-2.png" class="card--icon" />
+            <h3>Categorization</h3>
+            <p>
+              We find out the cause of the negative experience, and try to put
+              it in the appropriate box so you can gather stats.
+            </p>
+          </div>
+          <div className="info--card">
+            <img src="/icon-1.png" class="card--icon" />
+            <h3>Data Gathering</h3>
+            <p>
+              We collect as much relevant details from the user as possible
+              before the review happens.
+            </p>
+          </div>
+          <div className="info--card">
+            <img src="/icon-3.png" class="card--icon" />
+            <h3>Fast Resolution</h3>
+            <p>
+              We message the user immediately, apologising and asking for help
+              to resolve the issue.
+            </p>
+          </div>
+        </div>
+        <button className="button">
+          <span>LEARN MORE</span>
+        </button>
+      </div>
+    );
   }
 }
 
